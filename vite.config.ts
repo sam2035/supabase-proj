39a39104463cd1feb206450d65465d4131522e79
the sam <nbarbas.student@asiancollege.edu.ph>
2025-10-23 16:29:03 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// https://vitejs.dev/config/
+import * as path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
+})
